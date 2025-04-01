d050a52826bd823839c7e568b3934eec7760f9ce
hong tham <nguyenlehongtham2005@gmail.com>
2025-04-01 15:27:31 +0700

    sửa lại để wow hơn

--- myproject/myapp/static/js/script.js
+++ myproject/myapp/static/js/script.js
@@ -176,3 +176,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   
+document.getElementById("darkModeToggle").addEventListener("click", function() {
+  document.body.classList.toggle("dark-mode");
+});
